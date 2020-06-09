9f9bc3102e53ee4bf625e341fe0ef672fb19ce31
OpAlex8 <opalex1999@gmail.com>
2020-06-09 20:43:14 +0300

    added css files

--- frontend/src/PenguinBeautinator/PenguinBeautinator.js
+++ frontend/src/PenguinBeautinator/PenguinBeautinator.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './PenguinBeautinator.css';
+import Logo from "../Images/logo-transparent.png";
+import Gif from "../Images/giphy_transparent.gif"
 
 const PenguinBeautinator = () => {
     return (
         <div>
-            <h2>Despre noi</h2>
+            <img src={Logo} className={'homepage_Logo'} alt = ":("/>
+            <img src={Gif} className={'Homepage_Gif'} alt = ":("/>
             <p>Te-ai intrebat vreodata de ce pinguinii arata atat de bine si niciodata nu intarzie?<br/>
                 Ei bine, acum ai raspunsul!<br/>
                 Ei folosesc Penguin Beautinator ca sa fie mereu la timp si sa fie mereu imaculati</p>
