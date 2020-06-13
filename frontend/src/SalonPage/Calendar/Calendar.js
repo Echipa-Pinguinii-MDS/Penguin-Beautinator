bce61ff84691c61ae72ddf377db390bd41d7d5a5
Loila11 <miruna_z@yahoo.com>
2020-06-14 02:22:50 +0300

    Made weeks dynamic

--- frontend/src/SalonPage/Calendar/Calendar.js
+++ frontend/src/SalonPage/Calendar/Calendar.js
@@ -1,49 +1,22 @@
 import React from 'react';
+import {CalendarTitle, Week} from './Misc';
 import AvailableSlots from './AvailableSlots';
 import './Calendar.css'
-
-const Month = (props) => {
-    const months = ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie',
-        'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie']
-    return (
-        <div>
-            <h1>{months[props.month] + ' ' + props.year}</h1>
-        </div>
-    )
-}
-
-const Day = (props) => {
-    return (
-        <div className={'Day'} onClick={() => props.changeSelectedDate(props.dayNo)}>
-            <h1>{props.day}</h1>
-            <h1>{props.dayNo}</h1>
-        </div>
-    )
-}
-
-const Week = (props) => {
-    const days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica']
-    return (
-        <div className={'Week'}>
-            {days.map((day, index) =>
-                <Day key={index}
-                     day={day}
-                     dayNo={props.lastMonday + index}
-                     changeSelectedDate={props.changeSelectedDate}/>
-            )}
-        </div>
-    )
-}
 
 class Calendar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            lastMonday: new Date().getDate() - new Date().getDay() + (new Date().getDay() === 0 ? -6 : 1),
             day: new Date().getDate(),
             month: new Date().getMonth(),
-            year: new Date().getFullYear(),
+            year: new Date().getFullYear()
         }
         this.changeSelectedDate = this.changeSelectedDate.bind(this);
+        this.daysInMonth = this.daysInMonth.bind(this);
+        this.daysInPreviousMonth = this.daysInPreviousMonth.bind(this);
+        this.nextWeek = this.nextWeek.bind(this);
+        this.previousWeek = this.previousWeek.bind(this);
     }
 
     changeSelectedDate(day) {
@@ -54,14 +27,73 @@ class Calendar extends React.Component {
         this.props.setDate(day + '.' + this.state.month + '.' + this.state.year)
     }
 
+    daysInMonth(year, month) {
+        return new Date(year, month + 1, 0).getDate()
+    }
+
+    daysInPreviousMonth(year, month) {
+        if (month === 0) {
+            year -= 1
+            month = 11
+        } else {
+            month -= 1
+        }
+        return this.daysInMonth(year, month)
+    }
+
+    nextWeek() {
+        let daysInMonth = this.daysInMonth(this.state.year, this.state.month)
+        let endDay = new Date(this.state.year, this.state.month, daysInMonth).getDay()
+
+        if (this.state.lastMonday + 7 > daysInMonth) {
+            this.setState(state => ({
+                lastMonday: -endDay + 1,
+                day: 1,
+                month: (state.month + 1) % 12,
+                year: state.year + (state.month === 11)
+            }))
+        } else {
+            this.setState(state => ({
+                lastMonday: state.lastMonday + 7,
+                day: state.lastMonday + 7
+            }))
+        }
+    }
+
+    previousWeek() {
+        let daysInPreviousMonth = this.daysInPreviousMonth(this.state.year, this.state.month)
+
+        if (this.state.lastMonday <= 0) {
+            this.setState(state => ({
+                lastMonday: daysInPreviousMonth + state.lastMonday,
+                day: daysInPreviousMonth + state.lastMonday,
+                month: (state.month + 11) % 12,
+                year: state.year - (state.month === 0)
+            }))
+        } else if (this.state.lastMonday === 1) {
+            this.setState(state => ({
+                lastMonday: daysInPreviousMonth - 6,
+                day: daysInPreviousMonth - 6,
+                month: (state.month + 11) % 12,
+                year: state.year - (state.month === 0)
+            }))
+        } else {
+            this.setState(state => ({
+                lastMonday: state.lastMonday - 7,
+                day: Math.max(1, state.lastMonday - 7)
+            }))
+        }
+    }
+
     render() {
-        const date = new Date()
-        const lastMonday = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1)
         return (
             <div className={'Calendar'}>
-                <Month year={this.state.year}
-                       month={this.state.month}/>
-                <Week lastMonday={lastMonday}
+                <CalendarTitle year={this.state.year}
+                               month={this.state.month}
+                               previousWeek={this.previousWeek}
+                               nextWeek={this.nextWeek}/>
+                <Week lastMonday={this.state.lastMonday}
+                      daysInMonth={this.daysInMonth(this.state.year, this.state.month)}
                       date={this.state.day}
                       changeSelectedDate={this.changeSelectedDate}/>
                 {this.state.day < new Date().getDate() &&
---
+++ frontend/src/SalonPage/Calendar/Misc.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {GrNext, GrPrevious} from 'react-icons/gr';
+
+const Month = (props) => {
+    const months = ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie',
+        'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie']
+    return (
+        <div className={'Month'}>
+            <h2>{months[props.month]}</h2>
+            <h2>{props.year}</h2>
+        </div>
+    )
+}
+
+Month.propTypes = {
+    month: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired
+}
+
+export const CalendarTitle = (props) => {
+    return (
+        <div>
+            <GrPrevious onClick={props.previousWeek}>Saptamana anterioara</GrPrevious>
+            <Month month={props.month} year={props.year}/>
+            <GrNext onClick={props.nextWeek}>Saptamana viitoare</GrNext>
+        </div>
+    )
+}
+
+CalendarTitle.propTypes = {
+    month: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    previousWeek: PropTypes.func.isRequired,
+    nextWeek: PropTypes.func.isRequired
+}
+
+const Day = (props) => {
+    return (
+        <div className={'Day'} onClick={props.isClickable ? () => props.changeSelectedDate(props.dayNo) : null}>
+            <h1>{props.day}</h1>
+            <h1>{props.dayNo}</h1>
+        </div>
+    )
+}
+
+Day.propTypes = {
+    isClickable: PropTypes.bool.isRequired,
+    changeSelectedDate: PropTypes.func.isRequired,
+    day: PropTypes.string.isRequired,
+    dayNo: PropTypes.string.isRequired
+}
+
+export const Week = (props) => {
+    const days = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica']
+    return (
+        <div className={'Week'}>
+            {days.map((day, index) => {
+                let valid = 1 <= props.lastMonday + index && props.lastMonday + index <= props.daysInMonth
+                return (
+                    <Day key={index}
+                         day={day}
+                         dayNo={valid ? String(props.lastMonday + index) : '-'}
+                         isClickable={(valid)}
+                         changeSelectedDate={props.changeSelectedDate}/>
+                )
+            })}
+        </div>
+    )
+}
+
+Week.propTypes = {
+    lastMonday: PropTypes.number.isRequired,
+    daysInMonth: PropTypes.number.isRequired,
+    changeSelectedDate: PropTypes.func.isRequired
+}
